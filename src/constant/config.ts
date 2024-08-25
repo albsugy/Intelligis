d4d4cb26c4d3ea4d7bd472cb4d8796e5a76723cb
Medhat <medhat@getbaraka.com>
2024-08-25 15:36:17 +0400

    chore: setup auth

---
+++ src/constant/config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: 'Intelligis',
+  description: 'AI-powered project management platform',
+  url: 'https://intelligis.albsugy.com',
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/projects(.*)', '/forum(.*)']);
+const isProtectedRoute = createRouteMatcher([
+  '/projects(.*)',
+  '/compare(.*)',
+  '/factors(.*)',
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
